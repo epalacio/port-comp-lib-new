3729bb0ec1daa10477d39ddd150335ed3d7615e0
Esteban Palacio <estipalacio@gmail.com>
2021-01-28 14:14:29 -0500

    Added main title and theme colours through tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const App = () => {
   return (
-    <h1 className="text-yellow-900 text-5xl text-center">This is the first test</h1>
+    <div className="main-container">
+      <h1 className="">tailwindCSS</h1>
+      <h2>Component Library</h2>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        daintree: '#022534',
+        deepSeaGreen: '#094F6E',
+        jellyBean: '#2D7A9C',
+        sinbad: '#96C7D5',
+        lilyWhite: '#E4F5FF'
+      }
+    },
   },
   variants: {
     extend: {},
